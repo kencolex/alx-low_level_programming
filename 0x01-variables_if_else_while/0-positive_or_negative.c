b48fbc7fc674f8799a08289893b5be37ad428235
kencolex <ken_colex@yahoo.com>
2023-03-20 15:52:12 -0700

    update

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -17,15 +17,13 @@ int main(void)
 	{
 		printf("%d is positive\n", n);
 	}
-	else if (n < 0)
+	else if (n == 0)
         {
-		printf("%d is negative\n", n);
-	}
-	if (n = 0)
-	{
 		printf("%d is zero\n", n);
+	}
+	else
+	{
+		printf("%d is negative\n", n);
 	}
 	return (0);
 }
-
-
