f13ace32045733dafba5ef92d35e6370c49c3849
kencolex <ken_colex@yahoo.com>
2023-04-06 10:48:38 -0700

    update on factorial

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,9 +1,11 @@
 #include "main.h"
 
 /**
- * int factorial - will perform a factorial function
- * @n: the integer to perform the factorial on.
- * Return: factorial of n as integer
+ * factorial - gets factorial of n
+ * @n: integer
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
 int factorial(int n)
 {
